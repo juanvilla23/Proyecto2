7fbe60d833c0373a4b96e297c5bfd163cd254167
juanvilla23 <104267536+juanvilla23@users.noreply.github.com>
2022-05-21 17:39:11 -0500

    Add files via upload

---
+++ BichoNormal.java
@@ -0,0 +1,8 @@
+public class BichoNormal extends Bicho {
+    public BichoNormal(String nombre,int salud){
+        super(nombre,salud);
+    }
+    public BichoNormal(){
+        this("BN-",10);
+    }
+}
